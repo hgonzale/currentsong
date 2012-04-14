24d8c733422a30c8ee45bc4e303f47e49f9a1a3a
Humberto Gonzalez <hgonzale@eecs.berkeley.edu>
2012-04-13 23:31:52 -0700

    Initial Commit

---
+++ CurrentSong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CurrentSong
+//
+//  Created by Humberto on 04/13/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ CurrentSong/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  CurrentSong
+//
+//  Created by Humberto on 04/13/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
